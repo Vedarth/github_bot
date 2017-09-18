67434b9b2b0a105b030baceb13f8eb28d461c367
Vedarth <vedarth.sharma@gmail.com>
2017-09-18 20:10:29 +0530

    added oauth files and repository surfer

---
+++ gitsecrets.py
@@ -0,0 +1,9 @@
+# Keep this file separate
+
+# https://github.com/settings/applications
+# Create new App and get the two strings
+# Do not check thes in or reveal them to anyone
+
+def secrets():
+    return {"client_id": "6fc23ce79ddb2bc3ab48",
+            "client_secret": "a4285a8effa779bc48cd2e91b4db5f6724e70c1c"}
